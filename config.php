c733aebe2be76f3b4fc9d7e473b5635e60818d1d
MihaelaNicoleta <raumiha21@gmail.com>
2016-02-15 17:34:15 +0200

    add configuration file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+$client_secret = "CLIENT_SECRET";
+$client_id = "CLIENT_ID";
+
+$redirect_uri = "REDIRECT_URI";
+$scope = "SCOPE";
+
+$workspaces = array(
+                'workspace1' => "WORKSPACE1",
+                'workspace2' => "WORKSPACE2",
+                'workspace3' => "WORKSPACE3"
+            );
+
+//app_id - integer, not string
+$app_ids = array(
+                'app1' => app_id1,
+                'app2' => app_id2,
+                'app3' => app_id3
+            );
+
